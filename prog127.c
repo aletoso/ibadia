e49499f88aba25ffe8f1c2bb27e3753d05c34044
aletoso <64792003+aletoso@users.noreply.github.com>
2020-05-04 16:46:42 +0200

    Add files via upload

---
+++ prog127.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    FILE *pf;
+    
+    if((pf=fopen("127-out.txt","r"))==NULL) // abrir fichero en modo escritura
+        printf("ERROR AL ABRIR EL FICHERO");
+    
+    else
+    {
+        pf;
+    }
+    
+    
+} 
